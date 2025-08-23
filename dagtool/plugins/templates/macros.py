8145648e85d1b55b6e57aa29a6720eb880b1851a
Korawich Anuttra <korawich.anu@gmail.com>
2025-08-23 16:53:44 +0700

    :toolbox: build: upgrate docker file for build airflow standalone version.

--- dagtool/plugins/templates/macros.py
+++ dagtool/plugins/templates/macros.py
@@ -1,6 +1,12 @@
+import os
+
 from airflow.models import Variable
 
 
 def get_var(key: str) -> str | None:
     """Get Airflow Variable."""
     return Variable.get(key)
+
+
+def get_env(key: str) -> str | None:
+    return os.getenv(key, None)
